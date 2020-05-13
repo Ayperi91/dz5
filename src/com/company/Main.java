a959c9d6b477894e943fcaedd3406979f0acdbee
Ayperi91 <hayalim_2012@bk.ru>
2020-05-13 16:47:44 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Boss a = new Boss();
+        a.typeofProtection = "Manager";
+        a.health = 200;
+        a.damage = 50;
+        System.out.println( a.typeOfProtection + ""+a.health+ ""+a.damage);
+
+
+
+
+
+    }
+}
